93072a943f55f3494230f2f146a388eb3e3a50b9
Souvik Biswas <sbis1999@gmail.com>
2021-06-05 19:35:14 +0530

    Add initial welcome screen

---
+++ lib/presentation/screens/welcome_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:slibro/application/res/palette.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Palette.white,
+    );
+  }
+}
