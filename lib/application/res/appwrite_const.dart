24ec1a34f71a9240b48e7ebbba0d71dbae796509
Souvik Biswas <sbis1999@gmail.com>
2022-05-11 21:25:49 +0530

    Add appwrite constants

--- lib/application/res/appwrite_const.dart
+++ lib/application/res/appwrite_const.dart
@@ -1,1 +1,4 @@
 const storiesCollectionId = '627a37fd6c3524920a69';
+const filesBucketId = '627a52c3c13dedb31cbb';
+const authEmailSharedPrefKey = 'auth_email';
+const authPasswordSharedPrefKey = 'auth_password';
