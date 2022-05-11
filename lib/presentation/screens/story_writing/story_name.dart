d523cfb4ada7229f90020c07abfa1ff3a9c0270f
Souvik Biswas <sbis1999@gmail.com>
2022-05-11 21:28:04 +0530

    Update story name page

--- lib/presentation/screens/story_writing/story_name.dart
+++ lib/presentation/screens/story_writing/story_name.dart
@@ -1,11 +1,20 @@
+import 'package:appwrite/models.dart';
 import 'package:flutter/material.dart';
 import 'package:slibro/application/res/palette.dart';
 import 'package:slibro/presentation/screens/story_writing/chapter_description.dart';
 import 'package:slibro/presentation/screens/story_writing/writing_view.dart';
+import 'package:slibro/utils/database.dart';
 import 'package:slibro/utils/validators.dart';
 
 class StoryNameScreen extends StatefulWidget {
-  const StoryNameScreen({Key? key}) : super(key: key);
+  const StoryNameScreen({
+    Key? key,
+    required this.isShort,
+    required this.user,
+  }) : super(key: key);
+
+  final bool isShort;
+  final User user;
 
   @override
   State<StoryNameScreen> createState() => _StoryNameScreenState();
@@ -16,12 +25,22 @@ class _StoryNameScreenState extends State<StoryNameScreen> {
 
   late final TextEditingController _nameTextController;
   late final FocusNode _nameFocusNode;
+  late final DatabaseClient _databaseClient;
+  bool isStoring = false;
 
   @override
   void initState() {
     super.initState();
     _nameTextController = TextEditingController();
     _nameFocusNode = FocusNode();
+    _databaseClient = DatabaseClient();
+  }
+
+  @override
+  void dispose() {
+    _nameTextController.dispose();
+    _nameFocusNode.dispose();
+    super.dispose();
   }
 
   @override
@@ -51,7 +70,7 @@ class _StoryNameScreenState extends State<StoryNameScreen> {
                       fontSize: 16,
                       color: Palette.black,
                     ),
-                    textInputAction: TextInputAction.next,
+                    textInputAction: TextInputAction.done,
                     keyboardType: TextInputType.emailAddress,
                     cursorColor: Palette.greyMedium,
                     decoration: InputDecoration(
@@ -93,15 +112,49 @@ class _StoryNameScreenState extends State<StoryNameScreen> {
                     width: double.maxFinite,
                     child: ElevatedButton(
                       onPressed: () async {
-                        // TODO: Comment this out
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                const ChapterDescriptionScreen(),
-                          ),
-                        );
+                        _nameFocusNode.unfocus();
+                        if (_nameFormKey.currentState!.validate()) {
+                          setState(() {
+                            isStoring = true;
+                          });
+                          Document newStory = await _databaseClient.addStory(
+                            user: widget.user,
+                            storyName: _nameTextController.text,
+                            isShort: widget.isShort,
+                          );
+
+                          if (widget.isShort) {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => WritingScreen(
+                                  story: newStory,
+                                ),
+                              ),
+                            );
+                          } else {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const ChapterDescriptionScreen(),
+                              ),
+                            );
+                          }
+                          setState(() {
+                            isStoring = false;
+                          });
+                        }
                       },
-                      child: const Text('Continue to Chapters'),
+                      child: isStoring
+                          ? const SizedBox(
+                              height: 30,
+                              width: 30,
+                              child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                  Palette.greyDark,
+                                ),
+                              ),
+                            )
+                          : const Text('Continue to Chapters'),
                     ),
                   ),
                   const SizedBox(height: 30),
