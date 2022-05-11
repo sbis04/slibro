427b40c6c2060bfe1daca17eead7a4284fb79b0a
Souvik Biswas <sbis1999@gmail.com>
2022-05-11 21:26:40 +0530

    Add auto login to splash screen

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,9 +1,16 @@
 import 'dart:developer';
 
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:slibro/application/res/appwrite_const.dart';
 import 'package:slibro/application/res/palette.dart';
+import 'package:slibro/main.dart';
 import 'package:slibro/presentation/screens/login_screen.dart';
+
+import 'greet_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,6 +20,49 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool _isGettingUser = true;
+  String? _userID;
+
+  getLoggedInUserId() async {
+    setState(() {
+      _isGettingUser = true;
+    });
+
+    Account account = Account(client);
+
+    final prefs = await SharedPreferences.getInstance();
+    final String? userEmail = prefs.getString(authEmailSharedPrefKey);
+    final String? userPassword = prefs.getString(authPasswordSharedPrefKey);
+
+    if (userEmail != null && userPassword != null) {
+      final Session session = await account.createSession(
+        email: userEmail,
+        password: userPassword,
+      );
+
+      User loggedInUser = await account.get();
+
+      // TODO: Navigate to the dashboard
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => GreetScreen(
+            user: loggedInUser,
+          ),
+        ),
+      );
+    }
+
+    setState(() {
+      _isGettingUser = false;
+    });
+  }
+
+  @override
+  void initState() {
+    getLoggedInUserId();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
@@ -45,188 +95,33 @@ class _SplashScreenState extends State<SplashScreen> {
                 ],
               ),
             ),
-            // const CircularProgressIndicator(
-            //   valueColor: AlwaysStoppedAnimation<Color>(
-            //     Palette.greyDark,
-            //   ),
-            // ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Container(
+              child: SizedBox(
                 width: double.maxFinite,
                 child: ElevatedButton(
                   onPressed: () async {
                     Navigator.of(context).pushReplacement(
                       MaterialPageRoute(
-                        builder: (context) => LoginScreen(),
+                        builder: (context) => const LoginScreen(),
                       ),
                     );
                   },
-                  child: const Text('Get Started'),
+                  child: _isGettingUser
+                      ? const SizedBox(
+                          height: 30,
+                          width: 30,
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              Palette.greyDark,
+                            ),
+                          ),
+                        )
+                      : const Text('Get Started'),
                 ),
               ),
             ),
-            // FutureBuilder<User?>(
-            //   future: Authentication.initializeFirebase(),
-            //   builder: (context, snapshot) {
-            //     if (snapshot.connectionState == ConnectionState.done) {
-            //       User? currentUser = snapshot.data;
-
-            //       if (currentUser != null) {
-            //         WidgetsBinding.instance!.addPostFrameCallback((_) {
-            //           Navigator.of(context).pushReplacement(
-            //             MaterialPageRoute(
-            //               builder: (context) => WelcomeScreen(
-            //                 user: currentUser,
-            //               ),
-            //             ),
-            //           );
-            //         });
-
-            //         return Container();
-            //       } else {
-            //         return GoogleButton();
-            //       }
-            //     }
-            //     return CircularProgressIndicator(
-            //       valueColor: AlwaysStoppedAnimation<Color>(
-            //         Palette.greyDark,
-            //       ),
-            //     );
-            //   },
-            // ),
             const SizedBox(height: 42.0)
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class LoginSheet extends StatefulWidget {
-  const LoginSheet({Key? key}) : super(key: key);
-
-  @override
-  State<LoginSheet> createState() => _LoginSheetState();
-}
-
-class _LoginSheetState extends State<LoginSheet> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Palette.black,
-      child: DefaultTabController(
-        length: 2,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const TabBar(
-              tabs: [
-                Tab(icon: Text('Login')),
-                Tab(icon: Text('Create Account')),
-              ],
-            ),
-            SizedBox(
-              height: 280 + MediaQuery.of(context).viewInsets.bottom,
-              child: TabBarView(
-                children: [
-                  Form(
-                    // key: _loginFormKey,
-                    onChanged: () => setState(() {}),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const SizedBox(height: 16),
-                          TextFormField(
-                            // controller: _emailTextController,
-                            // focusNode: _emailFocusNode,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Palette.white,
-                            ),
-                            cursorColor: Theme.of(context).colorScheme.primary,
-                            decoration: InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  width: 3,
-                                ),
-                              ),
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color:
-                                      Theme.of(context).colorScheme.secondary,
-                                  width: 2,
-                                ),
-                              ),
-                              hintText: 'Enter email',
-                              hintStyle: TextStyle(
-                                fontSize: 16,
-                                color: Palette.white.withOpacity(0.6),
-                              ),
-                            ),
-                            // validator: (value) => Validators.validateEmail(
-                            //   email: value,
-                            // ),
-                            // onChanged: (value) => widget.onChange(value),
-                          ),
-                          const SizedBox(height: 24),
-                          TextFormField(
-                            obscureText: true,
-                            // controller: _passwordTextController,
-                            // focusNode: _passwordFocusNode,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Palette.white,
-                            ),
-                            cursorColor: Theme.of(context).colorScheme.primary,
-                            decoration: InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  width: 3,
-                                ),
-                              ),
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color:
-                                      Theme.of(context).colorScheme.secondary,
-                                  width: 2,
-                                ),
-                              ),
-                              hintText: 'Enter password',
-                              hintStyle: TextStyle(
-                                fontSize: 16,
-                                color: Palette.white.withOpacity(0.6),
-                              ),
-                            ),
-                            // validator: (value) => Validators.validatePassword(
-                            //   password: value,
-                            // ),
-                            // onChanged: (value) => widget.onChange(value),
-                          ),
-                          const SizedBox(height: 36),
-                          SizedBox(
-                            width: double.maxFinite,
-                            child: ElevatedButton(
-                              onPressed: () async {},
-                              child: const Text('Get Started'),
-                            ),
-                          ),
-                          const SizedBox(height: 32),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Icon(Icons.directions_transit),
-                ],
-              ),
-            ),
           ],
         ),
       ),
